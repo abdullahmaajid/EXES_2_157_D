b6da5362f51a18ae2e9021c9b6e5ee4e6903b6d9
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-05-12 09:28:44 +0700

    membuat fungsi array secara otomatis

--- EXES_2_157_D/EXES_2_157_D.cpp
+++ EXES_2_157_D/EXES_2_157_D.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 int main() {
-    int n = 57;
+
+    // Masukkan Jumlah Array yang telah ditentukan
+    int n = 57+12+20-(4x5)+10+5+2+3;
     int arr[n];
+
+    57+12+20-20+10+5+2+3
+    69+0+15+5
+    69 + 20 
+        = 89
+
+    // Mengisi array dengan angka secara otomatis
+    for (int i = 0; i < n; i++) {
+        arr[i] = i;
+        }
+
 
